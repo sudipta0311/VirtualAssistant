119afbc07c3fcac43abeaaf933464813e42ae8d6
sudipta0311 <43538281+sudipta0311@users.noreply.github.com>
2025-02-22 20:44:04 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ pc = Pinecone(api_key=PINECONE_API_KEY)
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 
 # vector store 
-index_name = "agent"
+index_name = "knowlodgebase"
 
 index = pc.Index(index_name)
 
