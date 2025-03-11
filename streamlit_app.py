c796dd8307e4aca1453c672e3adb6a1ed74c9af9
sudipta0311 <43538281+sudipta0311@users.noreply.github.com>
2025-03-11 20:22:32 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,9 @@ PINECONE_API_KEY = st.secrets.get("PINECONE_API_KEY")
 from langchain_openai import AzureChatOpenAI
 
 llm = AzureChatOpenAI(
-    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
-    azure_deployment=os.environ["AZURE_OPENAI_DEPLOYMENT_NAME"],
-    openai_api_version=os.environ["AZURE_OPENAI_API_VERSION"],
+    azure_endpoint=AZURE_OPENAI_ENDPOINT,
+    azure_deployment=AZURE_OPENAI_DEPLOYMENT_NAME,
+    openai_api_version=AZURE_OPENAI_API_VERSION,
 )
 
 import getpass
